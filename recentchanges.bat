43bc1246d43da800f37daa56cd48e2df9ceec7de
Colby Saigeon <csaig929@proton.me>
2026-03-14 02:09:07 -0700

    Add files via upload

--- recentchanges.bat
+++ recentchanges.bat
@@ -1,16 +1,5 @@
-
-
 @echo off
 
 rem rnt
 
-rem If no arguments were passed, add one for parser note for command line use
-if "%~1"=="" (
-    python "%~dp0src\set_recent_helper.py" run
-    exit /b
-)
-
-rem search
-
-rem If arguments were provided, prepend or call
 python "%~dp0src\set_recent_helper.py" %*
