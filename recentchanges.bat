aa681c5598990a5e790ca3fd9c0e95af43172b5d
Colby Saigeon <csaig929@proton.me>
2026-03-17 14:50:37 -0700

    Add files via upload

--- recentchanges.bat
+++ recentchanges.bat
@@ -1,5 +1,15 @@
 @echo off
 
-rem rnt
+set PYTHONUNBUFFERED="1"
+set "CMD_LINE=%~f0"
+rem If no arguments were passed, add one for parser note for command line use
+rem 300 seconds are used as no args means 5 minutes
+if "%~1"=="" (
+    python "%~dp0main.py" 300
+    exit /b
+)
 
-python "%~dp0src\set_recent_helper.py" %*
+rem search
+
+rem If arguments were provided, prepend or call
+python "%~dp0main.py" %*
--- rnt.bat
+++ rnt.bat
@@ -1,5 +1,15 @@
 @echo off
 
-rem rntexit /b
+rem rnt invert search switch
+set PYTHONUNBUFFERED="1"
+set "CMD_LINE=%~f0"
+rem If no arguments were passed, just prepend and call
+rem 300 seconds are used as no args means 5 minutes
+if "%~1"=="" (
+    python "%~dp0main.py" 300 inv
+    exit /b
+)
 
-python "%~dp0src\rntchanges.py" %* inv
+rem search
+rem If arguments were provided, prepend and pass arguments and call
+python "%~dp0main.py" %* inv
