8969b53f8c0f6ea780fcaae4656f1df7f0d17421
Colby Saigeon <csaig929@proton.me>
2026-07-25 13:16:08 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -216,6 +216,7 @@ class MainWindow(QMainWindow):
         self.proteusEXTN = ["[no extension]" if p == "" else p for p in proteusEXTN]
         self.proteusPATH = config['shield']['proteusPATH']
         self.checksum = config['diagnostics']['checkSUM']
+        self.checkMETHOD = config['diagnostics']['checkMETHOD']
         self.proteusSHIELD = config['shield']['proteusSHIELD']
         self.psEXEC = config['shield']['exec']
         self.exclDIRS = user_path(config['search']['exclDIRS'], usr)
@@ -1044,6 +1045,7 @@ class MainWindow(QMainWindow):
                 zipPATH = updated_config['compress']['zipPATH']
                 zipPROGRAM = updated_config['compress']['zipPROGRAM'].lower()
                 checksum = updated_config['diagnostics']['checkSUM']
+                checkMETHOD = updated_config['diagnostics']['checkMETHOD']
                 hudCOLOR = updated_config['display']['hudCOLOR']
                 hudSZE = updated_config['display']['hudSZE']
                 hudFNT = updated_config['display']['hudFNT']
@@ -1084,6 +1086,10 @@ class MainWindow(QMainWindow):
                 randintMAX = updated_config['calculator']['randintMAX']
                 randintMIN = updated_config['calculator']['randintMIN']
                 clogLEVEL = updated_config['calculator']['logLEVEL']
+
+                # added 07/24/2026
+                if checkMETHOD != self.checkMETHOD:
+                    self.ui.hudt.appendPlainText(f"checkMETHOD changed to {checkMETHOD} use recentchanges reset to start new db")
 
                 ll_level = self.config['logs']['logLEVEL']
                 new_ll_level = updated_config['logs']['logLEVEL']
@@ -1286,6 +1292,7 @@ class MainWindow(QMainWindow):
                 self.proteusEXTN = ["[no extension]" if p == "" else p for p in proteusEXTN]
                 self.proteusPATH = proteusPATH
                 self.checksum = checksum
+                self.checkMETHOD = checkMETHOD
                 self.proteusSHIELD = proteusSHIELD
                 self.psEXEC = psEXEC
                 self.exclDIRS = exclDIRS
